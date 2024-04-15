a0b69587f76e855f9139d4aab25520c50aaed773
김소희 <sohui0103@naver.com>
2024-04-16 00:53:06 +0900

    spring vue연동

---
+++ src/main/java/com/esg/esgProject/WebController.java
@@ -0,0 +1,14 @@
+package com.esg.esgProject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class WebController {
+    @GetMapping("/test")
+    public String test() {
+        return "Springboot와 Vue.js 연동 완료!";
+    }
+}
